e1e2778f9d3cc772e601f761caa65dbcbcdb6c80
Christopher Reinken <christopherreinken@gmail.com>
2020-03-29 04:49:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
-	    :rent_in_dollar => {}
+	    :rent_in_dollars => {}
 	  }
 	}
 
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
-	    :rent_in_dollar => {}
+	    :rent_in_dollars => {}
 	  }
 	}
 
